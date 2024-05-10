a0cb0974d1a6eb97e6156f7e105cf1b555c17abe
Theonlybeverly <140081447+Theonlybeverly@users.noreply.github.com>
2024-05-10 16:02:43 -0700

    Add files via upload

---
+++ beverlystreats.js
@@ -0,0 +1,13 @@
+let navbar = document.getElementById("mobile-navigation");
+
+function showNavbar(){
+    if (navbar.style.display ==="flex"){
+        navbar.style.display = "none";
+    }
+
+    else{
+        navbar.style.display ="flex";
+    }
+};
+
+
